15138897978d2cab5229fec4173191197db47443
Qiang Zhu <alecfans@gmail.com>
2018-10-25 00:22:54 -0700

    add test script

--- pyxtal_ml/test/test_all.py
+++ pyxtal_ml/test/test_all.py
@@ -1,1 +1,13 @@
-# a file to test all functions in the code.
+from pyxtal_ml.run import run
+from pkg_resources import resource_filename
+import os.path as op
+
+jsonfile = resource_filename("pyxtal_ml", "datasets/nonmetal_MP_8049.json")
+
+
+runner = run(N_sample=200, jsonfile=jsonfile)
+runner.load_data()
+runner.convert_data_1D()
+runner.ml_train()
+runner.print_outliers()
+
