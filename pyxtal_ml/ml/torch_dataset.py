187e0bfcedc8b98de8e521df8cd65de00f8d0c4c
yanxon <hg.yanxon@gmail.com>
2018-11-19 22:44:42 -0800

    DataLoader

--- pyxtal_ml/ml/torch_dataset.py
+++ pyxtal_ml/ml/torch_dataset.py
@@ -3,7 +3,7 @@ from torch.utils import data
 
 class dataset(data.Dataset):
     """
-    Building a dataset for PyTorch using
+    Initiating a dataset for PyTorch using
     DataLoader method.
 
     Args:
@@ -12,11 +12,11 @@ class dataset(data.Dataset):
     def __init__(self, xy):
         self.xy = xy
         self.len = xy.shape[0]
-        self.x_train = torch.from_numpy(self.xy[:, 0:-1])
-        self.y_train = torch.from_numpy(self.xy[:, [-1]])
+        self.X_train = torch.from_numpy(self.xy[:, 0:-1])
+        self.Y_train = torch.from_numpy(self.xy[:, [-1]])
 
     def __getitem__(self, index):
-        return self.x_train[index], self.y_train[index]
+        return self.X_train[index], self.Y_train[index]
 
     def __len__(self):
         return self.len
