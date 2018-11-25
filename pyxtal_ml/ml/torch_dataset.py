674c1af2dd5aa90aaacfca4172205890fc2901be
Howard Yanxon <32254481+yanxon@users.noreply.github.com>
2018-11-25 10:38:17 -0800

    add cnn

--- pyxtal_ml/ml/torch_dataset.py
+++ pyxtal_ml/ml/torch_dataset.py
@@ -20,3 +20,23 @@ class dataset(data.Dataset):
 
     def __len__(self):
         return self.len
+    
+class dataset_3d(data.Dataset):
+    """
+    Initiating a dataset for PyTorch using
+    DataLoader method.
+
+    Args:
+        x: training feature.
+        y: training property.
+    """
+    def __init__(self, x, y):
+        self.len = y.shape[0]
+        self.X_train = torch.from_numpy(x).float()
+        self.Y_train = torch.from_numpy(y).float()
+
+    def __getitem__(self, index):
+        return self.X_train[index], self.Y_train[index]
+
+    def __len__(self):
+        return self.len
