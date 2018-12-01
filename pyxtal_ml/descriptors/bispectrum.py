605fc53c660f6938f1e833ecda6221aa2e041eb9
David-Zagaceta <zagaceta@unlv.nevada.edu>
2018-12-01 00:24:17 -0800

    Added comments

--- pyxtal_ml/descriptors/bispectrum.py
+++ pyxtal_ml/descriptors/bispectrum.py
@@ -22,10 +22,6 @@ class Bispectrum(object):
 
         self._factorial = [1]
 
-        for i in range(int(3. * j_max) + 2):
-            if i > 0:
-                self._factorial += [i * self._factorial[i-1]]
-
         # symmetrize structure option
         if symmetrize:
             finder = SpacegroupAnalyzer(crystal, symprec=0.06,
@@ -48,15 +44,19 @@ class Bispectrum(object):
         '''
         self.bispectrum = []
         for index, site in enumerate(crystal):
+            # compute nearest neighbors using voronoi tesselations
             site_neighbors = get_neighbors_of_site_with_index(
                 crystal, index, approach='voronoi')
             # site loops over central atoms
             site_bispectrum = []
+            # iterate over 2*j_max
             for _2j1 in range(2 * self._j_max + 1):
                 j1 = _2j1/2
                 j2 = _2j1/2
+                # iterate over j_max -1
                 for j in range(int(min(_2j1, self._j_max,)) + 1):
-                    term = self.calculate_B(
+                    # calculate the real terms of the bispectrums
+                    term = self._calculate_B(
                         j1, j2, 1.0*j, site, site_neighbors)
                     term = term.real
                     site_bispectrum += [term]
@@ -64,11 +64,9 @@ class Bispectrum(object):
 
     @staticmethod
     def _m_values(k):
-        assert k >= 0
+        return np.arange(-k, k+1, 1)
 
-        return [k - i for i in range(int(2 * k + 1))]
-
-    def calculate_B(self, j1, j2, j, site, site_neighbors):
+    def _calculate_B(self, j1, j2, j, site, site_neighbors):
         '''
         Calculates the bispectrum coefficients associated with
         atomic site in a crystal structure
@@ -84,23 +82,35 @@ class Bispectrum(object):
         Returns:
             Bispectrum coefficients: a list of floats
         '''
+        # itiate the variable
         B = 0.
+        # compute the set of free integer parameters
         mvals = self._m_values(j)
+        # iterate over two free integer sets
         for m in mvals:
-            print(m)
             for m_prime in mvals:
-                c = self.calculate_c(j, m_prime, m, site, site_neighbors)
+                # see calculate c
+                c = self._calculate_c(j, m_prime, m, site, site_neighbors)
+                # assign new integer parameters, related to m and m prime
                 m1bound = min(j1, m + j2)
                 m_prime1_bound = min(j1, m_prime + j2)
                 m1 = max(-j1, m-j2)
+                '''
+                Iterate over a set of half integer values depending on the free
+                integer parameters m and m_prime
+                '''
                 while m1 < (m1bound + 0.5):
                     m_prime1 = max(-j1, m_prime - j2)
                     while m_prime1 < (m_prime1_bound + 0.5):
-                        c1 = self.calculate_c(j1, m_prime1, m1, site,
-                                              site_neighbors)
-                        c2 = self.calculate_c(j2, m_prime-m_prime1, m-m1, site,
-                                              site_neighbors)
+                        c1 = self._calculate_c(j1, m_prime1, m1, site,
+                                               site_neighbors)
+                        c2 = self._calculate_c(j2, m_prime-m_prime1, m-m1, site,
+                                               site_neighbors)
 
+                        '''
+                        here CG computes the Clebsch Gordon coefficients using functionality
+                        from sympy.
+                        '''
                         B += float(CG(j1, m1, j2, m-m1, j, m).doit()) * \
                             float(CG(j1, m_prime1, j2, m_prime-m_prime1, j, m_prime).doit()) * \
                             np.conjugate(c) * c1 * c2
@@ -109,23 +119,60 @@ class Bispectrum(object):
 
         return B
 
-    def calculate_c(self, j, m_prime, m, site, site_neighbors):
-        value = 0.
+    def _calculate_c(self, j, m_prime, m, site, site_neighbors):
+        '''
+        Calculate the inner product of the 4-D spherical harmonics associated
+        with an atomic site and integer/half-integer parameters
+
+        Args:
+            j, m_prime, m:  integer/half-integer parameters
+            site: a pymatgen periodic site object
+            site_neighbors: a list of pymatgen periodic site
+                            objects corresponding to the sites
+                            nearest neighbors
+
+        Returns:  the inner product of the harmonics associated with each neighbor
+        '''
+        # ititiate the data as a float
+        dot = 0.
+        '''
+        Calculate the 4-D spherical harmonic assoicated with each neigjbor/site pair
+        and add it to value to compute the inner product
+        '''
         for neighbor in site_neighbors:
+            '''
+            Calculate x, y, z, and the magnitude of the position vector
+            by calling the cartesian coordinates of the pymatgen
+            periodic site objects,  index 0 corresponds to x, 1 to y, and
+            2 to z
+            '''
             x = neighbor.coords[0] - site.coords[0]
             y = neighbor.coords[1] - site.coords[1]
             z = neighbor.coords[2] - site.coords[2]
             r = np.linalg.norm(neighbor.coords - site.coords)
+            '''
+            If the magnitude of the position vector is approximately greater
+            than zero compute the 4-D spherical harmonic associated with that site
+            if not continue.
+            '''
             if r > 10.**(-10.):
-
+                # first euler rotation angle
                 psi = np.arcsin(r / self._Rc)
 
-                theta = np.arccos(z / r)
+                '''
+                Second euler rotation angle, corresponds to the polar angle in
+                3-D spherical coordinates
+                '''
+                psi = np.arccos(z / r)
                 if abs((z / r) - 1.0) < 10.**(-8.):
                     theta = 0.0
                 elif abs((z / r) + 1.0) < 10.**(-8.):
                     theta = np.pi
 
+                '''
+                Third euler rotation angle, corresponds to the azimuthal angle in
+                3-D spherical coordinates
+                '''
                 if x < 0.:
                     phi = np.pi + np.arctan(y / x)
                 elif 0. < x and y < 0.:
@@ -139,29 +186,57 @@ class Bispectrum(object):
                 else:
                     phi = 0.
 
-                # populate this with atomic numbers
-                value += 1.0 * \
-                    np.conjugate(self.U(j, m, m_prime, psi, theta, phi)) * \
-                    self.cutoff_function(r, self._Rc)
+                '''
+                add the spherical harmonic multiplied by the cutoff function to the
+                inner product, the vectors considered are the complex numbers
+                associated with the spherical harmonic, the conjugate is the
+                scalar product
+                '''
+                dot += 1.0 * \
+                    np.conjugate(self._U(j, m, m_prime, psi, theta, phi)) * \
+                    self._cutoff_function(r, self._Rc)
 
             else:
                 continue
 
-        return value
+        return dot
 
-    def U(self, j, m, m_prime, psi, theta, phi):
-        result = 0.
+    def _U(self, j, m, m_prime, psi, theta, phi):
+        '''
+        Computes an element of the rotation group SO3
+        corresponding to integer / half integer values
+        j, m and m prime along with rotation angles psi,
+        theta, and phi
+
+        These elements are also the 4-D spherical harmonics
+
+        Args:
+            j: free integer parameter
+            m: fixed integer/half=integer parameter
+            m_prime: fixed integer/half-integer parameter
+            psi, theta, phi:  euler angles
+
+        Returns:  an element of the rotation group SO3, complex
+        '''
+        sph_harm = 0. + 0.j
         mvals = self._m_values(j)
         for mp in mvals:
-            result += complex(Rotation.D(S(int(2*j))/2, S(int(2*m))/2, S(int(2*mp))/2, phi, theta, -phi).doit()) * \
+            '''
+            The Rotation.D terms are the wigner D functions corresponding to certain integer/half integer values
+            and the rotation angles phi and theta.  The S terms in the argument are Sympy integer/half integer
+            objects for the calculation
+
+            This is the hilbert representation of the Rotation group SO3
+            '''
+            sph_harm += complex(Rotation.D(S(int(2*j))/2, S(int(2*m))/2, S(int(2*mp))/2, phi, theta, -phi).doit()) * \
                 np.exp(-1j * mp * psi) * \
                 complex(Rotation.D(S(int(2*j))/2, S(int(2*mp))/2,
                                    S(int(2*m_prime))/2, phi, -theta, -phi).doit())
 
-        return result
+        return sph_harm
 
     @staticmethod
-    def cutoff_function(r, rc):
+    def _cutoff_function(r, rc):
 
         if r > rc:
             return 0.
