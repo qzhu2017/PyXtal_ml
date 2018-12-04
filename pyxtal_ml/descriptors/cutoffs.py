df856b20286e4f27a0f81538cd45209ad8298788
yanxon <hg.yanxon@gmail.com>
2018-12-04 09:04:37 -0800

    cutoff functions

---
+++ pyxtal_ml/descriptors/cutoffs.py
@@ -0,0 +1,131 @@
+"""
+This is a cutoff script which contains several cutoff functions.
+
+All cutoff functions need a "todict" method.
+
+All cutoff functions also need to have an 'Rc' attribute which is the maximum
+distance at which properties are calculated; this will be used in calculating
+neighborlists.
+
+This script is adopted from AMP:
+    https://bitbucket.org/andrewpeterson/amp/src/2865e75a199a?at=master
+"""
+
+import numpy as np
+
+def dict2cutoff(dct):
+    """
+    This function converts a dictionary(which was created with the to_dict)
+    method of one of the cutoff classes) into an instantiated version of the
+    class. Modeled after ASE's dict2constraint function.
+    """
+    if len(dct) != 2:
+        raise RuntimeError('Cutoff dictionary must have only two values, '
+                           ' "name" and "kwargs".')
+    return globals()[dct['name']](**dct['kwargs'])
+
+class Cosine():
+    """
+    Cosine functional suggested by Behler.
+    
+    Args:
+        Rc(float): the maximum distance at which properties are calculated.
+    """
+    def __init__(self, Rc):
+        
+        self.Rc = Rc
+        
+    def __call__(self, Rij):
+        """
+        Args:
+            Rij(float): distance between pair atoms.
+            
+        Returns:
+            The value (float) of the cutoff function.
+        """
+        if Rij > self.Rc:
+            return 0.
+        else:
+            return (0.5 * (np.cos(np.pi * Rij / self.Rc) + 1.))
+        
+    def derivative(self, Rij):
+        """
+        Derivative (dF/dRij) of the Cosine function with respect to Rij.
+        
+        Args:
+            Rij(float): distance between pair atoms.
+            
+        Returns:
+            The derivative (float) of the Cosine function.
+        """
+        if Rij > self.Rc:
+            return 0.
+        else:
+            return (-0.5 * np.pi / self.Rc * np.sin(np.pi * Rij / self.Rc))
+        
+    def todict(self):
+        return {'name': 'Cosine',
+                'kwargs': {'Rc': self.Rc
+                           }
+                }
+        
+    def __repr(self):
+        return ('<Cosine cutoff with Rc=%.3f from descriptor.cutoffs>'
+                %self.Rc)
+        
+class Polynomial():
+    """
+    Polynomial functional suggested by Khorshidi and Peterson.
+    
+    Args:
+        alpha(float): the power of polynomial.
+        Rc(float): the maximum distance at which properties are calculated.
+    """
+    def __init__(self, Rc, alpha=4):
+        self.alpha = alpha
+        self.Rc = Rc
+        
+    def __call__(self, Rij):
+        """
+        Args:
+            Rij(float): distance between pair atoms.
+            
+        Returns:
+            The value (float) of the cutoff function.
+        """
+        if Rij > self.Rc:
+            return 0.
+        else:
+            value = 1. + self.alpha * (Rij / self.Rc) ** (self.alpha + 1) - \
+                (self.alpha + 1) * (Rij / self.Rc) ** self.alpha
+            return value
+        
+    def derivative(self, Rij):
+        """
+        Derivative (dF/dRij) of the Polynomial function with respect to Rij.
+        
+        Args:
+            Rij(float): distance between pair atoms.
+            
+        Returns:
+            The derivative (float) of the cutoff function.
+        """
+        if Rij > self.Rc:
+            return 0.
+        else:
+            ratio = Rij / self.Rc
+            value = (self.alpha * (self.alpha + 1) / self.Rc) * \
+                (ratio ** self.alpha - ratio ** (self.alpha - 1))
+        return value
+    
+    def todict(self):
+        return {'name': 'Polynomial',
+                'kwargs': {'Rc': self.Rc,
+                           'alpha': self.alpha
+                           }
+                }
+                
+    def __repr__(self):
+        return ('<Polynomial cutoff with Rc=%.3f and alpha=%i '
+                'from descriptor.cutoffs>'
+                %(self.Rc, self.alpha))
