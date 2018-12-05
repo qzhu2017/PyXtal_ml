c4c9b5416821ef3527bbdc7c9c00c94732782e59
Howard Yanxon <32254481+yanxon@users.noreply.github.com>
2018-12-05 15:56:05 -0800

    G2 implementation

--- pyxtal_ml/descriptors/gaussian.py
+++ pyxtal_ml/descriptors/gaussian.py
@@ -1,3 +1,17 @@
+import numpy as np
+from pymatgen.core.structure import Structure
+
+############################# Auxiliary Functions #############################
+
+def distance(arr):
+    """
+    L2 norm for cartesian coordinates
+    """
+    return ((arr[0] ** 2 + arr[1] ** 2 + arr[2] ** 2) ** 0.5)
+
+
+############################## Cutoff Functional ##############################
+
 """
 This script provides three cutoff functionals:
     1. Cosine
@@ -11,19 +25,6 @@ return zero if the radius is beyond Rc.
 This script is adopted from AMP:
     https://bitbucket.org/andrewpeterson/amp/src/2865e75a199a?at=master
 """
-
-import numpy as np
-
-def dict2cutoff(dct):
-    """
-    This function converts a dictionary(which was created with the to_dict)
-    method of one of the cutoff classes) into an instantiated version of the
-    class. Modeled after ASE's dict2constraint function.
-    """
-    if len(dct) != 2:
-        raise RuntimeError('Cutoff dictionary must have only two values, '
-                           ' "name" and "kwargs".')
-    return globals()[dct['name']](**dct['kwargs'])
 
 
 class Cosine(object):
@@ -76,10 +77,6 @@ class Cosine(object):
                 'kwargs': {'Rc': self.Rc
                            }
                 }
-        
-    def __repr(self):
-        return ('<Cosine cutoff with Rc=%.3f from descriptor.cutoffs>'
-                %self.Rc)
         
         
 class Polynomial(object):
@@ -137,12 +134,7 @@ class Polynomial(object):
                            'gamma': self.gamma
                            }
                 }
-                
-    def __repr__(self):
-        return ('<Polynomial cutoff with Rc=%.3f and gamma=%i '
-                'from descriptor.cutoffs>'
-                %(self.Rc, self.gamma))
-        
+                        
 
 class TangentH(object):
     """
@@ -195,7 +187,71 @@ class TangentH(object):
                 'kwargs': {'Rc': self.Rc
                            }
                 }
-        
-    def __repr(self):
-        return ('<TangentH cutoff with Rc=%.3f from descriptor.cutoffs>'
-                %self.Rc)
+
+############################# Symmetry Functions ##############################
+
+def calculate_G2(crystal, eta, cutoff_f='Cosine', Rc=6.5, Rs=0.0): # How do you pick eta?
+    """
+    Calculate G2 symmetry function.
+    G2 function is a better choice to describe the radial feature of atoms in
+    a crystal structure within the cutoff radius.
+    
+    One can refer to equation 9 in:
+    Behler, J. (2015). Constructing high‐dimensional neural network 
+    potentials: A tutorial review. 
+    International Journal of Quantum Chemistry, 115(16), 1032-1050.
+
+    Parameters
+    ----------
+    crystal: object
+        Pymatgen crystal structure object.
+    eta: float
+        The parameter of G2 symmetry function.
+    cutoff_f: str
+        Cutoff functional. Default is Cosine functional.
+    Rc: float
+        Cutoff radius which the symmetry function will be calculated.
+        Default value is 6.5 as suggested by Behler.
+    Rs: float
+        Determine the shift from the center of the Gaussian.
+        Default value is zero.
+
+    Returns
+    -------
+    G2 : float
+        G2 symmetry value.
+    """
+    # Cutoff functional
+    if cutoff_f == 'Cosine':
+        func = Cosine(Rc=Rc)
+    elif cutoff_f == 'Polynomial':
+        func = Polynomial(Rc=Rc)
+    elif cutoff_f == 'TangentH':
+        func = TangentH(Rc=Rc)
+    else:
+        raise NotImplementedError('Unknown cutoff functional: %s' %cutoff_f)
+    
+    # Get positions of core atoms
+    n_core = crystal.num_sites
+    n_core_cartesian = crystal.cart_coords
+    n_core_positions = np.apply_along_axis(distance, 1, n_core_cartesian)
+    
+    # Their neighbors within the cutoff radius
+    neighbors = crystal.get_all_neighbors(Rc)
+    n_neighbors = len(neighbors[1])
+    
+    G2 = []
+    for i in range(n_core):
+        G2_core = 0
+        for j in range(n_neighbors):
+            Rij = np.linalg.norm(n_core_positions[i] - neighbors[i][j][1])
+            G2_core += (np.exp(-eta * Rij ** 2.) / (Rc ** 2.) * 
+                        func(Rij))
+        G2.append(G2_core)
+    
+    return G2
+
+
+crystal = Structure.from_file('POSCARs/POSCAR-NaCl')
+x = calculate_G2(crystal, eta =2)
+print(x)
