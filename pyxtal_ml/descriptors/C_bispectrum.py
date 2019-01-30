fixed representation statistics and covariance

pyxtal_ml/descriptors/C_bispectrum.py
@@ -4,7 +4,6 @@ import ctypes
 from pymatgen.core.structure import Structure
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 from pymatgen.core.periodic_table import Element, Specie
-from pyxtal_ml.descriptors.stats import descriptor_stats
 import numpy as np
 from optparse import OptionParser
 import os
@@ -59,7 +58,7 @@ class C_Bispectrum(object):
 
         bispectrum = np.array(bispectrum)
 
-        self.bispectrum = descriptor_stats(bispectrum, axis=0).get_stats()
+        self.bispectrum = bispectrum
 
 
 if __name__ == "__main__":

pyxtal_ml/descriptors/bond_order_params.py
@@ -4,7 +4,6 @@ from pyxtal_ml.descriptors.angular_momentum import wigner_3j
 from pymatgen.core.structure import Structure
 from pymatgen.analysis.local_env import get_neighbors_of_site_with_index
 from scipy.special import sph_harm
-from pyxtal_ml.descriptors.stats import descriptor_stats
 from optparse import OptionParser
 
 def _qlm(site, neighbors, l, mvals):
@@ -182,7 +181,7 @@ class steinhardt_params(object):
 
         # call the values in the dictionary and insert them into a list
         parameters = np.array(list(bond_order_params.values()))
-        self.params = descriptor_stats(parameters, axis=1).get_stats()
+        self.params = parameters.T
 
     @staticmethod
     def _mvalues(l):

pyxtal_ml/descriptors/charge.py
@@ -1,7 +1,6 @@
 from pymatgen.core.structure import Structure
 import numpy as np
 from optparse import OptionParser
-from pyxtal_ml.descriptors.stats import descriptor_stats
 import os.path as op
 from monty.serialization import loadfn
 
@@ -20,7 +19,7 @@ class Charge(object):
         for k, v in el_dict.items():
             des = self.get_chgdescrp_arr(k)
             arr.append(des)
-        self.chg_stats = descriptor_stats(arr, axis=0).get_stats()
+        self.chg_stats = np.array(arr)
 
     def get_chgdescrp_arr(self, elm):
         d = ele_data[elm]

pyxtal_ml/descriptors/chem.py
@@ -1,7 +1,6 @@
 from monty.serialization import loadfn
 from pymatgen.core.structure import Structure
 import numpy as np
-from pyxtal_ml.descriptors.stats import descriptor_stats
 from optparse import OptionParser
 import os.path as op
 
@@ -26,7 +25,7 @@ class Chem(object):
         for k, v in el_dict.items():
             des = self.get_descrp_arr(k)
             arr.append(des)
-        self.chem_stats = descriptor_stats(arr, axis=0).get_stats()
+        self.chem_stats = np.array(arr)
 
     def get_descrp_arr(self, elm):
         # the current json file has only 82 elements, some are missing

pyxtal_ml/descriptors/descriptors.py
@@ -168,14 +168,25 @@ class descriptor:
 
         keys0, self.feature_counting = self.sort_features(keys0)
 
+        non_stats_keys = ['RDF', 'ADF', 'DDF', 'PRDF', 'cg', 'Voronoi', 'covariance']
         arr = []
         for key in keys0:
             if len(self.descriptor[key]) == 0:
                 print(key, np.shape(self.descriptor[key]))
             if len(arr) == 0:
-                arr = self.descriptor[key].flatten()
+
+                if key in non_stats_keys:
+                    arr = self.descriptor[key].flatten()
+
+                else:
+                    arr = descriptor_stats(self.descriptor[key]).get_stats().flatten()
             else:
-                arr = np.hstack((arr, self.descriptor[key].flatten()))
+
+                if key in non_stats_keys:
+                    arr = np.hstack((arr, self.descriptor[key].flatten()))
+
+                else:
+                    arr = np.hstack((arr, descriptor_stats(self.descriptor[key]).get_stats().flatten()))
         return arr
 
     def sort_features(self, features):

pyxtal_ml/descriptors/power_spectrum.py
@@ -2,7 +2,6 @@ import numpy as np
 from pymatgen.core.structure import Structure
 from pymatgen.analysis.local_env import get_neighbors_of_site_with_index
 import pyxtal_ml.descriptors.bond_order_params as bop
-from pyxtal_ml.descriptors.stats import descriptor_stats
 from optparse import OptionParser
 
 
@@ -38,8 +37,7 @@ class power_spectrum(object):
         '''
         If the list is 1 dimensional simply take the mean of the list
         If the list is 2 dimensional take the mean over the rows of the list'''
-        self.Power_spectrum = descriptor_stats(
-            spectrum_values, axis=1).get_stats()
+        self.Power_spectrum = np.array(spectrum_values).T
 
     def Pl(self, site, neighbors, l):
         '''