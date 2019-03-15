c472140d916eb18875dfed3ca3416a9dfcc69208
David-Zagaceta <zagaceta@unlv.nevada.edu>
2019-03-15 16:18:17 -0700

    finished bispectrum jitted functions stored in the cache

--- pyxtal_ml/descriptors/bispectrum.py
+++ pyxtal_ml/descriptors/bispectrum.py
@@ -4,11 +4,11 @@ from pymatgen.core.periodic_table import Element, Specie
 import numpy as np
 from angular_momentum import CG, wigner_D
 from optparse import OptionParser
-from numba import jit
+import numba
 
-
-@jit
-def _cutoff_function(r, rc):
+@numba.njit(numba.f8(numba.f8, numba.f8),
+            cache=True, nogil=True, fastmath=True)
+def cosine_cutoff(r, rc):
 
     if r > rc:
         return 0.
@@ -17,18 +17,226 @@ def _cutoff_function(r, rc):
         return 0.5 * (np.cos(np.pi * r / rc) + 1.)
 
 
+@numba.njit(numba.void(numba.i8, numba.f8[:,:,:,:,:]),
+            cache=True, nogil=True, fastmath=True)
+def populate_cg_array(j_max, in_arr):
+    '''
+    Populate a 5-D array with the Clebsch-Gordon coupling coefficients
+    given a j max value, this is intended for specific use with the bispectrum
+
+    The intent is to use this at the highest level of execution to minimize computation
+
+    We map the clebsch gordon arguments to array indeces using the following relations
+
+    CG(J1, M1, J2, M2, J, M)
+
+    Jn -> Jn/2
+
+    Mn -> Mn - Jn/2
+
+
+    args:
+        j_max: specify the degree of bispectrum calculation
+
+        in_arr: supplied array to insert CG coefficients
+    '''
+    # initiate a 5-d array with size 2*j_max+1
+    twojmax = 2*j_max
+    size = twojmax + 1
+    cgs = in_arr
+    # index j1 and j2 from 0 to 2jmax
+    for j1 in range(size):
+        for j2 in range(size):
+            # there exists a symmetry for j given by this array
+            js = np.arange(np.abs(j1-j2), min(twojmax, j1+j2) + 1, 2)
+            for j in js:
+
+                # index the sum from 0 to j1
+                for m1 in range(j1+1):
+                    aa2 = 2 * m1 - j1
+                    #index the sym from 0 to j2
+                    for m2 in range(j2+1):
+                        bb2 = 2*m2-j2
+
+                        # aa2 and bb2 ensure that m1 and m2 = m in the CG calc
+                        m = (aa2+bb2+j)/2
+
+                        '''
+                            restrict z-angular momentum to be a positive value
+                            not larger than total angular momentum
+                        '''
+                        if m < 0 or m > j:
+                            continue
+
+                        # convert array arguments to CG args
+                        J1 = j1/2
+                        J2 = j2/2
+                        J = j/2
+
+                        M1 = m1 - J1
+                        M2 = m2 - J2
+                        M = m - J
+                        # add CG coef to cgs array
+                        cgs[j1,j2,j,m1,m2] = CG(J1, M1, J2, M2, J, M)
+
+
+@numba.njit(numba.c16(numba.i8, numba.i8, numba.i8,
+                      numba.f8, numba.f8, numba.f8),
+            cache=True, fastmath=True, nogil=True)
+def U(j, m, m_prime, psi, theta, phi):
+    '''
+    Computes the 4-D hyperspherical harmonic given the three angular coordinates
+    and indeces
+    '''
+    j = j/2
+    m = m - j
+    m_prime = m_prime - j
+    sph_harm = 0. + 0.j
+    mvals = np.arange(-j, j+1, 1)
+    for mp in mvals:
+        sph_harm += wigner_D(j, m, mp, phi, theta, -phi) * \
+                np.exp(-1j * mp * psi) * \
+                wigner_D(j, mp, m_prime, phi, -theta, -phi)
+
+    return sph_harm.conjugate()
+
+
+@numba.njit(numba.c16(numba.i8, numba.i8, numba.i8,
+                      numba.f8[:,:], numba.f8[:], numba.f8[:]),
+            cache=True, fastmath=True, nogil=True)
+def compute_C(j, mp, m, hypersphere_coords, rbf_vals, cutoff_vals):
+    '''
+    Computes the inner product of the 4-D hyperspherical harmonics
+    radial basis function values and cutoff function values
+
+    Args:
+        j, mp, m:  indeces for 4-D hyperspherical harmonics
+    '''
+    dot = 0
+    for i in range(len(hypersphere_coords)):
+        psi = hypersphere_coords[i,0]
+        theta = hypersphere_coords[i,1]
+        phi = hypersphere_coords[i,2]
+        harmonic = U(j, m, mp, psi, theta, phi)
+        dot += harmonic*rbf_vals[i]*cutoff_vals[i]
+
+    return dot
+
+
+@numba.njit(numba.void(numba.i8, numba.c16[:,:,:], numba.f8[:,:],
+                       numba.f8[:], numba.f8[:]),
+            cache=True, fastmath=True, nogil=True)
+def populate_C_array(jmax, in_arr, hypersphere_coords, rbf_vals, cutoff_vals):
+    '''
+    Populates the array of the inner products from compute_C
+
+    args:
+        jmax: degree of bispectrum calculation
+        in_arr: reference to array to populate
+        hypersphere_coords: 2-D array of psis, thetas, phis from 4-d spherical coordinate system
+        rbf_vals: 1-D array of radial basis function values
+        cutoff_vals: 1-D array of cutoff function values
+
+    note that these arrays should be the same length
+    '''
+
+    twojmax = 2*jmax
+    size = twojmax + 1
+    cs = in_arr
+
+    for j in range(size):
+        ms = np.arange(0, j+1, 1)
+        for ma in ms:
+            for mb in ms:
+                cs[j, mb, ma] = compute_C(j, mb, ma, hypersphere_coords, rbf_vals, cutoff_vals)
+
+
+
+
+@numba.njit(numba.void(numba.i8, numba.f8[:,:,:,:,:],
+                       numba.c16[:,:,:], numba.c16[:,:,:,:,:]),
+            cache=True, fastmath=True, nogil=True)
+def compute_z_array(jmax, cgs, cs, in_arr):
+    '''
+    Precomputes the last two sums in the bispectrum
+
+    args:
+        jmax: degree of bispectrum
+        cgs:  5-D array of clebsch gordon coefficients
+        cs: 3D array of the inner products of the 4-D hyperspherical
+            harmonics, radial basis function, and cutoff function
+        in_arr: 5-D array for sums
+'''
+
+
+    twojmax = 2*jmax
+    size = twojmax + 1
+    zs = in_arr
+    cgs = cgs
+    cs = cs
+
+    for j1 in range(size):
+        for j2 in range(j1+1):
+            js = np.arange(j1-j2, min(twojmax, j1+j2) + 1, 2)
+            for j in js:
+                mbs = np.arange(0, j/2 + 1, 1)
+                for mb in mbs:
+                    for ma in range(j+1):
+                        ma1s = np.arange(max(0, (2*ma-j-j2+j1)/2),
+                                         min(j1, (2*ma-j+j2+j1)/2) + 1,
+                                         1)
+
+                        for ma1 in ma1s:
+                            sumb1 = 0. + 0.j
+                            ma2 = (2 * ma- j - (2 * ma1 - j1) + j2) / 2
+
+                            mb1s = np.arange(max(0, (2*mb-j-j2+j1)/2),
+                                             min(j1, (2*mb-j+j2+j1)/2) + 1,
+                                             1)
+
+                            for mb1 in mb1s:
+                                mb2 = (2 * mb - j - (2* mb1 - j1) + j2) / 2
+                                sumb1 += cgs[int(j1),int(mb1),int(j2),int(mb2),int(j)] * cs[int(j1),int(ma1),int(mb1)] * cs[int(j2),int(ma2),int(mb2)]
+
+                            zs[int(j1),int(j2),int(j),int(ma),int(mb)] += sumb1*cgs[int(j1),int(ma1),int(j2),int(ma2),int(j)]
+
+
+@numba.njit(numba.void(numba.i8, numba.c16[:,:,:],
+                       numba.c16[:,:,:,:,:], numba.c16[:,:,:]),
+            cache=True, nogil=True, fastmath=True)
+def compute_bispectrum(jmax, cs, zs, in_arr):
+    '''
+    Computes the bispectrum
+
+    args:
+        jmax: degree of bispectrum
+        cs: 3D array of precomued inner products of
+            hyperspherical harmonics, radial basis function,
+            and cutoff function
+        zs: 5-D array of pre computed sums (see SNAP)
+    '''
+
+    twojmax = 2*jmax
+    size = twojmax + 1
+    bis = in_arr
+
+    for j1 in range(size):
+        for j2 in range(size):
+            js = np.arange(np.abs(j1-j2), min(twojmax, j1+j2) + 1, 2)
+            for j in js:
+                mbs = np.arange(0, j/2 + 1, 1)
+                for mb in mbs:
+                    for ma in range(j+1):
+                        c = cs[int(j),int(ma),int(mb)]
+                        bis[int(j1),int(j2),int(j)] += 2*c.conjugate()*zs[int(j1),int(j2),int(j),int(ma),int(mb)]
+
+
 class Bispectrum(object):
 
-    @jit(parallel=True)
-    def __init__(self, crystal, j_max=5, cutoff_radius=6.5, symmetrize=True):
-        '''
-        '''
+    def __init__(self, crystal, j_max=5, cutoff_radius=6.5, symmetrize=True, CG_coefs=None):
 
         # populate private attributes
         self._j_max = j_max
-        self._Rc = cutoff_radius
-
-        self._factorial = [1]
 
         # symmetrize structure option
         if symmetrize:
@@ -37,40 +245,86 @@ class Bispectrum(object):
             crystal = finder.get_conventional_standard_structure()
 
         '''
-        Here we iterate over each site and its corresponding neighbor
-        environment to compute the bispectrum coefficients of each
-        site-neighbor interaction.
+        Here we find all of the neighbors up to the specified cutoff radius,
+        then we transform those x, y, and z coordinates to a 4-D hyperspherical
+        coordinate system: r, psi, theta, phi.  Where r is the ordinary position
+        vector magnitude, psi, theta and phi are angular coordinates defined in the
+        code below.
 
-        At each site, the bispectrum is a list where the list elements are
-        the bispectrum corresponds each to a site-neighbor interaction
-
-        In turn the descriptor is effectively a list of lists, where the
-        element lists contain the bispectrum coefficients of the neighbor
-        environment
+        The code below populates arrays for the hypersphere angular coordinates,
+        the radial cutoff function value, and the atomic numbers at each site, and stores
+        them in an attribute.
         '''
-        self.bispectrum = []
-        neighbors = crystal.get_all_neighbors(self._Rc)
+        # neighbor calculation
+        neighbors = crystal.get_all_neighbors(cutoff_radius)
+        hypersphere_coords = []  # psi, theta, phi angles
+        AN = []  # atomic numbers of sites
+        cutoff_vals = [] # cutoff function values (radial component)
         for index, site in enumerate(crystal):
-            site_neighbors = neighbors[index]
-            # site loops over central atoms
-            site_bispectrum = []
-            # iterate over 2*j_max
-            for _2j1 in range(2 * self._j_max + 1):
-                j1, j2 = _2j1/2, _2j1/2
-                # iterate over j_max -1
-                for j in range(int(min(_2j1, self._j_max,)) + 1):
-                    # calculate the real terms of the bispectrums
-                    term = self._calculate_B(
-                        j1, j2, 1.0*j, site, site_neighbors)
-                    term = term.real
-                    site_bispectrum += [term]
-            self.bispectrum += [site_bispectrum]
+            '''
+            first dimension corresponds to site,
+            second dimension corresponds to neighbor
+            third dimension corresponds to coordinates / values
+            '''
+            hypersphere_coords.append([])
+            AN.append([])
+            cutoff_vals.append([])
 
-    @staticmethod
-    def _m_values(k):
-        return np.arange(-k, k+1, 1)
+            for neighbor in neighbors[index]:
+                '''
+                Each neighbor in neighbors is a tuple of length 2
 
-    @jit(parallel=True)
+                The item at the zero index in the tuple is the pymatgen
+                periodic site object
+
+                The item at the one index in the tuple is the euclidian
+                distance from the center site to the neighbor site
+                '''
+
+                # cartesian coordinates
+                x, y, z = neighbor[0].coords - site.coords
+
+                # euclidean vector norm
+                r = neighbor[1]
+
+                # do not populate the attribute for small r
+                if r < 10.**(-10):
+                    continue
+
+                psi = np.arcsin(r / cutoff_radius)
+
+                theta = np.arccos(z / r)
+                if abs((z / r) - 1.0) < 10.**(-8.):
+                    theta = 0.0
+                elif abs((z / r) + 1.0) < 10.**(-8.):
+                    theta = np.pi
+
+                if x < 0.:
+                    phi = np.pi + np.arctan(y / x)
+                elif 0. < x and y < 0.:
+                    phi = 2 * np.pi + np.arctan(y / x)
+                elif 0. < x and 0. <= y:
+                    phi = np.arctan(y / x)
+                elif x == 0. and 0. < y:
+                    phi = 0.5 * np.pi
+                elif x == 0. and y < 0.:
+                    phi = 1.5 * np.pi
+                else:
+                    phi = 0.
+
+                # [index] corresponds to a periodic site
+                hypersphere_coords[index].append([psi, theta, phi])
+                AN[index].append([neighbor[0].specie.number])
+
+                # eventually we may want to consider different symmetry functions
+                cutoff_vals[index].append([cosine_cutoff(r, cutoff_radius)])
+
+        # populate the attributes
+        self.hypersphere_coords = np.array(hypersphere_coords)
+        self.atomic_numbers = np.array(AN)
+        self.cutoff_vals = np.array(cutoff_vals)
+        self.CGs = CG_coefs
+
     def _calculate_B(self, j1, j2, j, site, site_neighbors):
         '''
         Calculates the bispectrum coefficients associated with
@@ -120,117 +374,6 @@ class Bispectrum(object):
 
         return B
 
-    @jit(parallel=True)
-    def _calculate_c(self, j, m_prime, m, site, site_neighbors):
-        '''
-        Calculate the inner product of the 4-D spherical harmonics associated
-        with an atomic site and integer/half-integer parameters
-
-        Args:
-            j, m_prime, m:  integer/half-integer parameters
-            site: a pymatgen periodic site object
-            site_neighbors: a list of pymatgen periodic site
-                            objects corresponding to the sites
-                            nearest neighbors
-
-        Returns:  the inner product of the harmonics associated with each neighbor
-        '''
-        # ititiate the data as a float
-        dot = 0.
-        '''
-        Calculate the 4-D spherical harmonic assoicated with each neigjbor/site pair
-        and add it to value to compute the inner product
-        '''
-        for neighbor in site_neighbors:
-            '''
-            Calculate x, y, z, and the magnitude of the position vector
-            by calling the cartesian coordinates of the pymatgen
-            periodic site objects,  index 0 corresponds to x, 1 to y, and
-            2 to z
-            '''
-            x = neighbor[0].coords[0] - site.coords[0]
-            y = neighbor[0].coords[1] - site.coords[1]
-            z = neighbor[0].coords[2] - site.coords[2]
-            r = neighbor[1]  # distance is stored here
-            ele = neighbor[0].specie
-            G = ele.number
-            '''
-            If the magnitude of the position vector is approximately greater
-            than zero compute the 4-D spherical harmonic associated with that site
-            if not continue.
-            '''
-            if r > 10.**(-10.):
-                # first euler rotation angle
-                psi = np.arcsin(r / self._Rc)
-
-                '''
-                Second euler rotation angle, corresponds to the polar angle in
-                3-D spherical coordinates
-                '''
-                theta = np.arccos(z / r)
-                if abs((z / r) - 1.0) < 10.**(-8.):
-                    theta = 0.0
-                elif abs((z / r) + 1.0) < 10.**(-8.):
-                    theta = np.pi
-
-                '''
-                Third euler rotation angle, corresponds to the azimuthal angle in
-                3-D spherical coordinates
-                '''
-                if x < 0.:
-                    phi = np.pi + np.arctan(y / x)
-                elif 0. < x and y < 0.:
-                    phi = 2 * np.pi + np.arctan(y / x)
-                elif 0. < x and 0. <= y:
-                    phi = np.arctan(y / x)
-                elif x == 0. and 0. < y:
-                    phi = 0.5 * np.pi
-                elif x == 0. and y < 0.:
-                    phi = 1.5 * np.pi
-                else:
-                    phi = 0.
-
-                '''
-                add the spherical harmonic multiplied by the cutoff function to the
-                inner product, the vectors considered are the complex numbers
-                associated with the spherical harmonic, the conjugate is the
-                scalar product
-                '''
-                dot += G * \
-                    np.conjugate(self._U(j, m, m_prime, psi, theta, phi)) * \
-                    _cutoff_function(r, self._Rc)
-
-            else:
-                continue
-
-        return dot
-
-    @jit(parallel=True)
-    def _U(self, j, m, m_prime, psi, theta, phi):
-        '''
-        Computes an element of the rotation group SO3
-        corresponding to integer / half integer values
-        j, m and m prime along with rotation angles psi,
-        theta, and phi
-
-        These elements are also the 4-D spherical harmonics
-
-        Args:
-            j: free integer parameter
-            m: fixed integer/half=integer parameter
-            m_prime: fixed integer/half-integer parameter
-            psi, theta, phi:  euler angles
-
-        Returns:  an element of the rotation group SO3, complex
-        '''
-        sph_harm = 0. + 0.j
-        mvals = self._m_values(j)
-        for mp in mvals:
-            sph_harm += wigner_D(j, m, mp, phi, theta, -phi) * \
-                np.exp(-1j * mp * psi) * \
-                wigner_D(j, mp, m_prime, phi, -theta, -phi)
-
-        return sph_harm
 
 
 if __name__ == "__main__":
@@ -246,7 +389,11 @@ if __name__ == "__main__":
     else:
         fileformat = 'poscar'
 
-    test = Structure.from_file(options.structure)
+    #test = Structure.from_file(options.structure)
 
-    f = Bispectrum(test, j_max=1, cutoff_radius=6.5)
-    print(f.bispectrum)
+    #f = Bispectrum(test, j_max=1, cutoff_radius=6.5)
+    #print(f.bispectrum)
+    jmax = 1
+    in_arr = np.zeros([2*jmax+1]*5)
+    x = populate_cg_array(jmax, in_arr)
+    print(in_arr)
